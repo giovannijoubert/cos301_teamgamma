86f1611f60989e1177f82c38f796acbd873bab39
AaronFacoline <u13283881@tuks.co.za>
2020-03-12 11:27:05 +0200

    Creating neural networks java project

---
+++ NeuralNetwork/src/main/java/ga/teamgamma/ai/NeuralNetwork/NeuralNetworkApplication.java
@@ -0,0 +1,13 @@
+package ga.teamgamma.ai.NeuralNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralNetworkApplication.class, args);
+	}
+
+}
---
+++ NeuralNetwork/src/test/java/ga/teamgamma/ai/NeuralNetwork/NeuralNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.teamgamma.ai.NeuralNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
