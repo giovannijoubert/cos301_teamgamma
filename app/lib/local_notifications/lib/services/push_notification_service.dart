f1846b9fc1449cc076ed9afc95a8479b775fc07a
Tlou Lebelo <u15209190@tuks.co.za>
2020-03-13 10:55:47 +0200

    my commit

---
+++ app/lib/local_notifications/lib/services/push_notification_service.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+
+class PushNotificationService {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        _navigateToItemDetail(message);
+      },
+    );
+
+}
+
+void setup(){
+  
+}
