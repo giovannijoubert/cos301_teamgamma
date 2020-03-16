134bfe5296ff4ab481bee10f1bd703b6f8c0c3d8
AaronFacoline <u13283881@tuks.co.za>
2020-03-16 12:38:25 +0200

    Java Spring Boot server project created

---
+++ server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetworksApplication.java
@@ -0,0 +1,13 @@
+package ga.teamgamma.ai.NeuralNetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralNetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralNetworksApplication.class, args);
+	}
+
+}
---
+++ server/neuralnetwork/Neural-Networks/src/test/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.teamgamma.ai.NeuralNetworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralNetworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
