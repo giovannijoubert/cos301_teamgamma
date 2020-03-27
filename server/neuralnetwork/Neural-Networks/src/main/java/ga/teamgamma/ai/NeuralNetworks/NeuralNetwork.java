2b4a51a17b2845252dece2fd43f583816e070cb0
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2020-03-27 20:02:09 +0200

    Commented out bloat in neural network, implemented Train function

--- server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetwork.java
+++ server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetwork.java
@@ -25,51 +25,6 @@ public class NeuralNetwork
 	int width = 100;
 	int numLabels = 12;
 
-	public static boolean convertDataToTestCSV() {
-	  return true;
-	}
-
-	public static String normalizeTestCSV() {
-	return "success";
-	}
-
-	//with data we get from conversion team in JSON obj, we take 2/3 of the data and put it into TrainingCSV
-	public static boolean convertDataToTrainingCSV() {
-	  return true;
-	}
-
-	public static String normalizeTrainingCSV() {
-	  return "success";
-	}
-
-	public static boolean recordReaderTraining() {
-	  return true;
-	}
-
-	public static boolean recordReaderTest() {
-	  return true;
-	}
-
-	public static boolean dataSetIteratorTraining() {
-	  return true;
-	}
-
-	public static boolean dataSetIteratorTest() {
-	  return true;
-	}
-
-	public static String multilayer() {
-	  return "created";
-	}
-
-	public static String trainingModel() {
-	  return "accurate";
-	}
-
-	public static String evaluateModel() {
-	  return "trained";
-	}
-
 	public static String exportModel() {
 	  return "model exported";
 	}
@@ -146,4 +101,53 @@ public class NeuralNetwork
 
 		return new MultiLayerNetwork(conf);
 	}
+
+
+/**
+ * //under review
+	public static boolean convertDataToTestCSV() {
+		return true;
+	}
+
+	public static String normalizeTestCSV() {
+		return "success";
+	}
+
+	//with data we get from conversion team in JSON obj, we take 2/3 of the data and put it into TrainingCSV
+	public static boolean convertDataToTrainingCSV() {
+		return true;
+	}
+
+	public static String normalizeTrainingCSV() {
+		return "success";
+	}
+
+	public static boolean recordReaderTraining() {
+		return true;
+	}
+
+	public static boolean recordReaderTest() {
+		return true;
+	}
+
+	public static boolean dataSetIteratorTraining() {
+		return true;
+	}
+
+	public static boolean dataSetIteratorTest() {
+		return true;
+	}
+
+	public static String multilayer() {
+		return "created";
+	}
+
+	public static String trainingModel() {
+		return "accurate";
+	}
+
+	public static String evaluateModel() {
+		return "trained";
+	}
+*/
 }
---
+++ server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/Training.java
@@ -0,0 +1,96 @@
+package ga.teamgamma.ai.NeuralNetworks;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class Training {
+    private MultiLayerNetwork model = null;
+
+    /**
+     * TRAINING
+     * */
+    private int numRows = 100,
+            numCols = 100,
+            outputNum = 12,
+            numSamples = 36,//3 samples per formant, 12 formants
+            batchSize = 100;
+
+    /**
+     * PRE-TRAINING
+     */
+    private int NUM_ROWS = 28,
+                NUM_COLUMNS = 28,
+                OUTPUT_NUMS = 12,//number of output classes
+                BATCH_SIZE = 64,//batch size for each epoch
+                RNG_SEED = 123,
+                NUM_EPOCHS = 15;//number of epochs to perform
+    private  double RATE= 0.0015;//learning rate
+    DataSetIterator trainingIter;
+    private Random rng = new Random(RNG_SEED);
+    public Training(){
+
+    }
+
+    public MultiLayerNetwork train(boolean pretrain, MultiLayerNetwork model){
+        try{
+            return pretrain ?
+                    train(model, new MnistDataSetIterator(BATCH_SIZE,true, RNG_SEED))
+                    : train(model,getDataSetIteratorFromFile());
+
+        }catch(Exception e){
+            System.err.println("Error Training: "+e.getMessage());
+        }
+        return null;
+    }
+
+    private DataSetIterator getDataSetIteratorFromFile() throws IOException {
+        File parentPath = new File(System.getProperty("user.dir"), "/pathToTrainingDataFolder/");
+        ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+        FileSplit fileSplit = new FileSplit(parentPath, NativeImageLoader.ALLOWED_FORMATS, rng);
+        BalancedPathFilter pathFilter = new BalancedPathFilter(rng, labelMaker, numSamples, outputNum, batchSize);
+
+        ImageRecordReader recordReader = new ImageRecordReader(numRows,numCols,outputNum,labelMaker);
+        recordReader.initialize(fileSplit);
+        return new RecordReaderDataSetIterator(recordReader,batchSize,1,outputNum);
+    }
+
+    private MultiLayerNetwork train(MultiLayerNetwork model, DataSetIterator data) throws IOException{
+        model.init();
+        model.setListeners(new ScoreIterationListener(5));//print the score
+
+
+        if(new File("../NeuralNetworkConfiguration").exists()){
+            model = ModelSerializer
+                    .restoreMultiLayerNetwork(
+                            new File("../NeuralNetworkConfiguration")
+                    );
+        }else{
+            for (int i = 0; i < NUM_EPOCHS; i++){
+                model.fit(data);
+                ModelSerializer
+                        .writeModel(
+                                model,
+                                new File( "../NeuralNetworkConfiguration"),
+                                true
+                        );
+            }
+        }
+
+        return model;
+    }
+}
