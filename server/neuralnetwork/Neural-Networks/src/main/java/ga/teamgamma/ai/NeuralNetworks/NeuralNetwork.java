9a499fcf561020f3407e03f93684f7d799f70384
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2020-03-31 11:26:37 +0200

    Started Improved Neural Network Documentation

--- server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetwork.java
+++ server/neuralnetwork/Neural-Networks/src/main/java/ga/teamgamma/ai/NeuralNetworks/NeuralNetwork.java
@@ -32,138 +32,235 @@ public class NeuralNetwork
 	@SuppressWarnings("SameParameterValue")
 	private ConvolutionLayer convInit(String name, int in, int out, int[] kernel, int[] stride, int[] pad, double bias)
 	{
-		return new ConvolutionLayer.Builder(kernel, stride, pad).name(name).nIn(in).nOut(out).biasInit(bias).build();
+		return new ConvolutionLayer
+									.Builder(
+												kernel,	//kernel size
+												stride,	//stride
+												pad		//padding		-
+											)
+									.name(name)			//name of the layer
+									.nIn(in)			//number of inputs to the layer (input size)
+									.nOut(out)			//number of outputs of the layer(output size)
+									.biasInit(bias)		//bias
+									.build();
 	}
 
 	private ConvolutionLayer conv3x3(String name, int out, double bias)
 	{
-		return new ConvolutionLayer.Builder(new int[]{3,3}, new int[] {1,1}, new int[] {1,1}).name(name).nOut(out).biasInit(bias).build();
+		return new ConvolutionLayer
+									.Builder(
+												new int[]{3,3},	//kernel size
+												new int[] {1,1},//stride
+												new int[] {1,1}	//padding
+												)
+									.name(name)					//name of the layer
+									.nOut(out)					//number of outputs of the layer(output size)
+									.biasInit(bias)				//bias
+									.build();
 	}
 
 	@SuppressWarnings("SameParameterValue")
 	private ConvolutionLayer conv5x5(String name, int out, int[] stride, int[] pad, double bias)
 	{
-		return new ConvolutionLayer.Builder(new int[]{5,5}, stride, pad).name(name).nOut(out).biasInit(bias).build();
+		return new ConvolutionLayer
+									.Builder(
+												new int[]{5,5},	//kernel size
+												stride,			//string
+												pad				//padding
+											)
+									.name(name)					//layer name
+									.nOut(out)					//number of outputs of the layer(output size)
+									.biasInit(bias)				//bias
+									.build();
 	}
 
 	private SubsamplingLayer maxPool(String name, int[] kernel)
 	{
-		return new SubsamplingLayer.Builder(kernel, new int[]{2,2}).name(name).build();
+		return new SubsamplingLayer
+									.Builder(
+												kernel,			//kernel size
+												new int[]{2,2}	//filter
+												)
+									.name(name)					//layer name
+									.build();
 	}
 
 	@SuppressWarnings("SameParameterValue")
 	private DenseLayer fullyConnected(String name, int out, double bias, double dropOut, Distribution dist)
 	{
-		return new DenseLayer.Builder().name(name).nOut(out).biasInit(bias).dropOut(dropOut).weightInit(new WeightInitDistribution(dist)).build();
+		return new DenseLayer
+							.Builder()
+							.name(name)
+							.nOut(out)
+							.biasInit(bias)
+							.dropOut(dropOut)
+							.weightInit(new WeightInitDistribution(dist))
+							.build();
 	}
 
 	private MultiLayerNetwork alexnetModel()
 	{
+		/**
+		 * THINGS WE CAN CHANGE:
+		 * ---------------------
+		 * 1.) The number of layers
+		 * 2.) The number of kernels
+		 * 3.) Kernel sizes
+		 * 4.) Number of neurons in fully connected layers
+		 * 5.) Other regularization methods like L1/L2
+		 * */
 		double bias = 1;
 		double dropOut = 0.5;
 
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(seed) // constant seed
-				.weightInit(new NormalDistribution(0.0, 0.01)) //add weights to all the nodes
-				.activation(Activation.RELU)
+				.weightInit(
+						new NormalDistribution(
+								0.0,
+								0.01
+						)
+				) //add weights to all the nodes
+				.activation(
+						Activation.RELU									//activation function is RELU
+				)
 				.updater(new AdaDelta())
 				.gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
 				.l2(0.0005) //5 * 1e-4
 				.list()
-				//Large 11x11 window to capture objects
-				//Stride of 4 to reduce size
-				//Channels = 1 due to grey-scale
-				//96 = ?
-				.layer(convInit("cnn1", channels, 96, new int[]{11, 11}, new int[]{4, 4}, new int[]{3, 3}, 0))
-				.layer(new LocalResponseNormalization.Builder().name("lrn1").build())
-				.layer(maxPool("maxpool1", new int[]{3,3}))
-				//Make the convolutional window smaller
-				//Set padding to 2 for consistent height/width
-				//256 = ?
-				.layer(conv5x5("cnn2", 256, new int[] {1,1}, new int[] {2,2}, bias))
-				.layer(new LocalResponseNormalization.Builder().name("lrn2").build())
-				.layer(maxPool("maxpool2", new int[]{3,3}))
-				//Use three successive convolutional layers and a smaller convolution window
-				//384 = ?
-				//256 = ?
-				.layer(conv3x3("cnn3", 384, 0))
-				.layer(conv3x3("cnn4", 384, bias))
-				.layer(conv3x3("cnn5", 256, bias))
+				/**====================================================================/
+				/**Convolutional Layer 1*/
+				.layer(
+						convInit(
+								"cnn1",
+								1,									//channels - 1 because grey scale.
+							96,									//filters - 96 (of size 11x11) WHY!!!!
+								new int[]{11, 11},						//kernel - 11x11 window to capture objects
+								new int[]{4, 4},						//stride - 4 to reduce size
+								new int[]{3, 3},						//padding
+							0										//bias - 0 for no bias
+						)
+				)
+				/**Normalization Layer 1*/
+				.layer(
+						new LocalResponseNormalization
+													.Builder()
+													.name("lrn1")
+													.build()
+				)
+				/**Max-Pooling Layer 1*/
+				.layer(
+						maxPool(
+								"maxpool1",						//
+								new int[]{3,3}							//pooling size - 3x3
+								)
+				)
+				/**====================================================================/
+				/**Convolutional Layer 2*/
+				.layer(
+						conv5x5(
+								"cnn2",							//
+								256,								//filters - 256 ?
+								new int[] {1,1},						//stride -  (1x1)
+								new int[] {2,2},						//padding - 2x2 for consistent height/width
+								bias									//bias -
+						)
+				)
+				/**Normalization Layer 2*/
+				.layer(
+						new LocalResponseNormalization
+													.Builder()
+													.name("lrn2")
+													.build()
+				)
+				/**Max-Pooling Layer 2*/
+				.layer(
+						maxPool(
+								"maxpool2",
+								new int[]{3,3}							//pool size - 3x3
+								)
+				)
+				/**====================================================================*/
+				/**Convolutional Layer 3 */
+				.layer(
+						conv3x3(
+								"cnn3",
+								384,							//output size/number of outputs of the layers -
+								0								//bias - 0 for no bias
+						)
+				)
+				/**====================================================================*/
+				/**Convolutional Layer 4 */
+				.layer(
+						conv3x3(
+								"cnn4",
+								384,							//output size/number of outputs of the layers -
+								bias								//bias -
+						)
+				)
+				/**====================================================================*/
+				/**Convolutional Layer 5 */
+				.layer(
+						conv3x3(
+								"cnn5",
+								256,							//output size/number of outputs of the layers -
+								bias								//bias -
+						)
+				)
 				//Reduce dimentionality
-				.layer(maxPool("maxpool3", new int[]{3,3}))
-				//Expensice dense layer
-				//4096 = ?
-				.layer(fullyConnected("ffn1", 4096, bias, dropOut,  new NormalDistribution(0, 0.005)))
-				.layer(fullyConnected("ffn2", 4096, bias, dropOut, new NormalDistribution(0, 0.005)))
+				.layer(
+						maxPool(
+								"maxpool3",
+								new int[]{3,3}						//pool size - 3x3
+								)
+				)
+				/**====================================================================*/
+				/**Convolutional Layer 6 */
+				.layer(
+						fullyConnected(
+								"ffn1",
+								4096,							//output size/number of outputs of the layers
+								bias,								//bias
+								dropOut,							//drop out
+								new NormalDistribution(
+										0,
+										0.005
+								)
+						)
+				)
+				/**====================================================================*/
+				/**Convolutional Layer 7 */
+				.layer(
+						fullyConnected(
+								"ffn2",
+								4096,							//output size/number of outputs of the layers
+								bias,								//bias -
+								dropOut,							//drop out -
+								new NormalDistribution(
+										0,
+										0.005
+								)
+						)
+				)
+				/**====================================================================*/
+				/**Output Layer*/
 				//Output layer
-				.layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-						.name("output")
-						.nOut(numLabels) //12 Mouth Shapes
-						.activation(Activation.SOFTMAX)
-						.build())
-				.setInputType(InputType.convolutional(height, width, channels))
+				.layer(
+						new OutputLayer
+										.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+										.name("output")
+										.nOut(numLabels) //12 Mouth Shapes
+										.activation(Activation.SOFTMAX)
+										.build())
+				/**====================================================================*/
+				.setInputType(
+								InputType.convolutional(
+										height,
+										width,
+										channels
+								)
+				)
 				.build();
 
 		return new MultiLayerNetwork(conf);
 	}
-
-
-/*
-	//under review
-	public static boolean convertDataToTestCSV()
- 	{
-		return true;
-	}
-
-	public static String normalizeTestCSV()
- 	{
-		return "success";
-	}
-
-	//with data we get from conversion team in JSON obj, we take 2/3 of the data and put it into TrainingCSV
-	public static boolean convertDataToTrainingCSV()
- 	{
-		return true;
-	}
-
-	public static String normalizeTrainingCSV()
- 	{
-		return "success";
-	}
-
-	public static boolean recordReaderTraining()
- 	{
-		return true;
-	}
-
-	public static boolean recordReaderTest()
-	{
-		return true;
-	}
-
-	public static boolean dataSetIteratorTraining()
-	{
-		return true;
-	}
-
-	public static boolean dataSetIteratorTest()
-	{
-		return true;
-	}
-
-	public static String multilayer()
-	{
-		return "created";
-	}
-
-	public static String trainingModel()
-	{
-		return "accurate";
-	}
-
-	public static String evaluateModel()
-	{
-		return "trained";
-	}
-*/
 }
