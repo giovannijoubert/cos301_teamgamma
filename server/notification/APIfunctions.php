45232b3c1279a46603ac7d1f6e9ef4feb2179588
Jaynill Gopal <u15175295@tuks.co.za>
2020-05-05 00:37:17 +0200

    Functionality Added and web UI

---
+++ server/notification/APIfunctions.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * FUNCTIONS
+ */
+/**
+ * @param int $type : 1=send email, 2=send network notification, 3 or any other value = send both
+ * @param $user : user's email address. May be array: user[0]=email, user[1]=deviceID
+ * @param string $msg : message to send
+ * @param $subject : heading for notification
+ * @param string $deviceID : client's device ID. if empty, function defaults to email only
+ */
+
+function sendNotification($type="", $user="", $msg="", $subject="", $deviceID="")
+{
+    if ($deviceID = "" || $type == 1) {
+        sendEmail($user, $msg, $subject);
+    } elseif ($type == 2) {
+        sendNetworkNotification($deviceID,$msg,$subject);
+    } else {
+        sendEmail($user, $msg, $subject);
+        sendNetworkNotification($deviceID,$msg,$subject);
+    }
+    /*
+    if(array_key_exists('testEmail', $_POST)) {
+        sendEmail();
+    }
+    else if(array_key_exists('testJSON', $_POST)) {
+        loadJson();
+    }
+    else if(array_key_exists('testBOTH', $_POST)) {
+        sendEmail();
+        loadJson();
+    }*/
+}
+
+/**
+ * @param string $email client's email address
+ * @param string $msg : message that is entered or can manually be retieved from the template messages, edit if necessary
+ * @param string $subject : Email subject to send
+ * @param bool $network : N/A changed to seperate function
+ */
+function sendEmail($email = "", $msg = "", $subject = "", $network = false)
+    //if (isset("testEmail",$_POST))
+{
+    if ($email == "") {
+        echo json_encode(array("Message"=> "Please enter email as 1st param"));
+        die("Please enter email as 1st param");
+    } else if ($msg == "") {
+        echo json_encode(array("Message"=> "Please enter message as 2nd param"));
+        die("Please enter email as 2nd param");
+    } else {
+        $emailaddr = 'u15175295@tuks.co.za';
+        /*$msg = "Congratulations! You have successfully signed up for MouthPiece\n
+            Have fun with our range of exciting mouthpieces to play with.\n
+            With love from the MouthPiece Team :)";*/
+
+        $msg = wordwrap($msg, 70);
+
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: <mail.teamgamma.ga>' . "\r\n";
+        $headers .= 'Cc: mail.teamgamma.ga' . "\r\n";
+        mail($email, $subject, $msg, $headers);
+
+    }
+}
+
+/**
+ * @param string $deviceID : get from user profile, Cant test it out properly
+ * @param string $message : message to send in notification
+ * @return bool|string: dw about it
+ */
+
+function sendNetworkNotification($deviceID = "", $message = "",$subject)
+{
+  //  echo "<b><i>Sending Network notification</i></b>";
+    if ($message == "") {
+        echo json_encode(array("Message"=> "Please enter email as 2nd param"));
+        die("Please enter email as 2nd param");
+    }
+    $url = 'https://fcm.googleapis.com/fcm/send';
+
+    $fields = array(
+        'registration_ids' => array($deviceID),
+        'priority' => "high",
+        'data' => array(
+            "message" => $message
+        )
+    );
+    $api_key = "AAAALQ1KC18:APA91bGg7iuimXlkzx90JtVrLHLyIbbMuWGRaBcYoG8IpiK-hqQKVhvoj3KafCQfvNDnQ7sK9blEBWz3GPXkX86Fe-tjXf2fsNHHjw3gaY9Z59iViVuZcSPmB_AP1KsuQv11cP6p0JFA";
+    // "BETr3AbYVksWTs_JIoHuuNnp9lfp3q9f1E9fBebkV6MQNMYuLWWWyOepQ9rEFUYWIYnb02XKcddQp7_9D2XCrr0";
+
+    $headers = array(
+        'Content-Type:application/json',
+        'Authorization:key=' . $api_key
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('FCM Send Error: ' . curl_error($ch));
+        echo json_encode(array("Message"=>('FCM Send Error: ' . curl_error($ch))));
+    }
+    curl_close($ch);
+    echo $result;
+    return $result;
+
+    /*
+curl -X POST --header "Authorization: key=BETr3AbYVksWTs_JIoHuuNnp9lfp3q9f1E9fBebkV6MQNMYuLWWWyOepQ9rEFUYWIYnb02XKcddQp7_9D2XCrr0" \
+--Header "Content-Type: application/json" \
+https://fcm.googleapis.com/fcm/send \
+-d "{\"to\":\"1:193496484703:android:dff32eba757777540184ea\",\"notification\":{\"body\":\"Yellow\"},\"priority\":10}"
+*/
+}
--- server/notification/notificationAPI.php
+++ server/notification/notificationAPI.php
@@ -1,5 +1,14 @@
 <?php
-
+    require ('APIfunctions.php');
+    /** PARAMETERS:
+     * 1. key: required = API key
+     * 2. getMessages (**optional** type must be sent if getMessages not sent) = for retrieving template data
+     * 2. type (**optional** getMessages must be sent if type not sent ) = Message type: email, push, both
+     * 3. email (optional) = User's email
+     * 4. deviceID (optional) = User's device ID
+     * 5. msg (required if type is sent) = Message body of notification
+     * 6. heading (required if type is sent)
+     */
     if (isset($_POST["key"]))//=="3e30630d-239b-488d-8938-b9305dff3e54"))//&& trim($_GET["q"]) == 'link1');
     {
        // session_start();
@@ -26,25 +35,28 @@
                // $email = $_POST['email'];
                 if ($notificationType === 1 && !isset($_POST['email']))
                 {
-
-                    echo json_encode(array("Message"=>"Error! Please Enter an email address"));
+                    header("HTTP/1.1 400 Bad Request",true,400);
+                    header('Content-Type: text/json');
+                    echo json_encode(array("message"=>"Error! Please Enter an email address"));
                 }
                 else if ($notificationType === 2 && !isset($_POST['deviceID']))
                 {
-                    echo "Error! Please Enter a device ID";
+                    header("HTTP/1.1 400 Bad Request",true,400);
+                    header('Content-Type: text/json');
+                    echo json_encode(array( "Error! Please Enter a device ID"));
                 }
                 else if ($notificationType > 2) //Both
                 {
                     if (!isset($_POST['email']))
                     {
-                        header("HTTP/1.1 200 Success",true,200);
+                        header("HTTP/1.1 400 Bad Request",true,400);
                         header('Content-Type: text/json');
-                        echo json_encode(array("Message"=> "Error! Please Enter an email address"));
+                        echo json_encode(array("message"=> "Error! Please Enter an email address"));
                     }else
                     if (!isset($_POST['deviceID'])) {
-                        header("HTTP/1.1 200 Success",true,200);
+                        header("HTTP/1.1 400 Bad Request",true,400);
                         header('Content-Type: text/json');
-                        echo json_encode(array("Message"=> "Error! Please Enter a device ID"));
+                        echo json_encode(array("message"=> "Error! Please Enter a device ID"));
                     }
                     else{
                         $email = $_POST['email'];
@@ -53,6 +65,9 @@
                         $heading = $_POST['heading'];
 
                         sendNotification($notificationType, $email, $message, $heading, $deviceID);
+                        header("HTTP/1.1 200 Success",true,200);
+                        header('Content-Type: text/json');
+                        echo json_encode(array("message"=> "Success! Notification sent successfully"));
                     }
                 } else {
                     $email = $_POST['email'];
@@ -61,10 +76,15 @@
                     $heading = $_POST['heading'];
 
                     sendNotification($notificationType, $email, $message, $heading, $deviceID);
+                    header("HTTP/1.1 200 Success",true,200);
+                    header('Content-Type: text/json');
+                    echo json_encode(array("message"=> "Success! Notification sent successfully"));
                 }
             }
             else{
-                echo json_encode(array("Message"=> "What's happening?"));
+                header("HTTP/1.1 400 Bad Request",true,400);
+                header('Content-Type: text/json');
+                echo json_encode(array("message"=> "What's happening?"));
             }
 
             /**
@@ -74,137 +94,16 @@
              * echo "<br>";
              */
 
-            /**
-             * FUNCTIONS
-             */
-            /**
-             * @param int $type : 0=send email, 1=send network notification, 2 or any other value = send both
-             * @param $user : user's email address. May be array: user[0]=email, user[1]=deviceID
-             * @param string $msg : message to send
-             * @param $subject : heading for notification
-             * @param string $deviceID : client's device ID. if empty, function defaults to email only
-             */
-
-            function sendNotification($type = 3, $user, $msg = "Generic Template message that goes here", $subject = "Generic Message", $deviceID = "")
-            {
-                if ($deviceID = "" || $type == 0) {
-                    sendEmail($user, $msg, $subject);
-                } elseif ($type == 1) {
-                    sendNetworkNotification();
-                } else {
-                    sendEmail($user, $msg, $subject);
-                    sendNetworkNotification();
-                }
-                /*
-                if(array_key_exists('testEmail', $_POST)) {
-                    sendEmail();
-                }
-                else if(array_key_exists('testJSON', $_POST)) {
-                    loadJson();
-                }
-                else if(array_key_exists('testBOTH', $_POST)) {
-                    sendEmail();
-                    loadJson();
-                }*/
-            }
-
-            /**
-             * @param string $email client's email address
-             * @param string $msg : message that is entered or can manually be retieved from the template messages, edit if necessary
-             * @param string $subject : Email subject to send
-             * @param bool $network : N/A changed to seperate function
-             */
-            function sendEmail($email = "", $msg = "", $subject = "", $network = false)
-                //if (isset("testEmail",$_POST))
-            {
-                if ($email == "") {
-                    echo json_encode(array("Message"=> "Please enter email as 1st param"));
-                    die("Please enter email as 1st param");
-                } else if ($msg == "") {
-                    echo json_encode(array("Message"=> "Please enter message as 2nd param"));
-                    die("Please enter email as 2nd param");
-                } else {
-                    $emailaddr = 'u15175295@tuks.co.za';
-                    /*$msg = "Congratulations! You have successfully signed up for MouthPiece\n
-                        Have fun with our range of exciting mouthpieces to play with.\n
-                        With love from the MouthPiece Team :)";*/
-
-                    $msg = wordwrap($msg, 70);
-
-                    $headers = "MIME-Version: 1.0" . "\r\n";
-                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-                    // More headers
-                    $headers .= 'From: <mail.teamgamma.ga>' . "\r\n";
-                    $headers .= 'Cc: mail.teamgamma.ga' . "\r\n";
-                    mail($email, $subject, $msg, $headers);
-                }
-            }
-
-            /**
-             * @param string $deviceID : get from user profile, Cant test it out properly
-             * @param string $message : message to send in notification
-             * @return bool|string: dw about it
-             */
-
-            function sendNetworkNotification($deviceID = "", $message = "")
-            {
-                echo "<b><i>Sending Network notification</i></b>";
-                if ($message == "") {
-                    echo json_encode(array("Message"=> "Please enter email as 2nd param"));
-                    die("Please enter email as 2nd param");
-                }
-                $url = 'https://fcm.googleapis.com/fcm/send';
-
-                $fields = array(
-                    'registration_ids' => array($deviceID),
-                    'priority' => "high",
-                    'data' => array(
-                        "message" => $message
-                    )
-                );
-                $api_key = "AAAALQ1KC18:APA91bGg7iuimXlkzx90JtVrLHLyIbbMuWGRaBcYoG8IpiK-hqQKVhvoj3KafCQfvNDnQ7sK9blEBWz3GPXkX86Fe-tjXf2fsNHHjw3gaY9Z59iViVuZcSPmB_AP1KsuQv11cP6p0JFA";
-                // "BETr3AbYVksWTs_JIoHuuNnp9lfp3q9f1E9fBebkV6MQNMYuLWWWyOepQ9rEFUYWIYnb02XKcddQp7_9D2XCrr0";
-
-                $headers = array(
-                    'Content-Type:application/json',
-                    'Authorization:key=' . $api_key
-                );
-
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-                $result = curl_exec($ch);
-                if ($result === FALSE) {
-                    die('FCM Send Error: ' . curl_error($ch));
-                    echo json_encode(array("Message"=>('FCM Send Error: ' . curl_error($ch))));
-                }
-                curl_close($ch);
-                echo $result;
-                return $result;
-
-                /*
-            curl -X POST --header "Authorization: key=BETr3AbYVksWTs_JIoHuuNnp9lfp3q9f1E9fBebkV6MQNMYuLWWWyOepQ9rEFUYWIYnb02XKcddQp7_9D2XCrr0" \
-            --Header "Content-Type: application/json" \
-            https://fcm.googleapis.com/fcm/send \
-        -d "{\"to\":\"1:193496484703:android:dff32eba757777540184ea\",\"notification\":{\"body\":\"Yellow\"},\"priority\":10}"
-            */
-            }
         }
         else{
             header("HTTP/1.1 401 Unauthorized",true,401);
             //header('Content-Type: text/json');
-            echo json_encode(array("Message"=> "Incorrect API key"));
+            echo json_encode(array("message"=> "Incorrect API key"));
         }
 
     }
     else{
         header("HTTP/1.1 403 Forbidden",true,403);
         //header('Content-Type: text/json');
-        echo json_encode(array("Message"=> "Error Incorrect parameters"));
+        echo json_encode(array("message"=> "Error Incorrect parameters"));
     }
